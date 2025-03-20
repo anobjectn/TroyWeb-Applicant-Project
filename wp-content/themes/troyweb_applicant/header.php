73c9bc15b1350a0f22b5a56f1ac52f18550a976d
Luis Ortiz <luis@overit.com>
2025-03-20 17:01:56 -0400

    applicant-single header mobile updates

--- wp-content/themes/troyweb_applicant/header.php
+++ wp-content/themes/troyweb_applicant/header.php
@@ -37,7 +37,8 @@
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,700;1,700&family=Oldenburg&family=Oregano:ital@0;1&display=swap" rel="stylesheet">
+        <!-- <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,700;1,700&family=Oldenburg&family=Oregano:ital@0;1&display=swap" rel="stylesheet"> -->
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,700;0,900;1,700&family=Oldenburg&family=Oregano:ital@0;1&display=swap" rel="stylesheet">
         <?php wp_head(); ?>
         
     </head>
